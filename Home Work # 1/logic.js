ddc70ca72b86369594e84c7ab0f5fbedd611cb60
Roman <yreymak@gmail.com>
2018-10-23 01:33:14 +0300

    21696$

--- Home Work # 1/logic.js
+++ Home Work # 1/logic.js
@@ -1,5 +1,41 @@
-function processing(){
+
+/*function processing(){
     var tempt = document.getElementById("inpur").value;
-    alert(tempt);
+    return tempt;
     
 }
+
+function createBut(){
+count="c";
+var value = document.createElement("<button >apply</button>");
+value.id=count;
+var colo=document.getElementById("down");
+colo.insertAdjacentElement("beforeEnd",value);
+}
+
+
+"<button style='border:4px solid black'>apply</button>"
+
+*/
+function tr(){
+   alert("start");
+   poz.document.getElementsByClassName("down");
+   var valur=document.createElement('button');
+   //var valur=document.createElement('div');
+   valur.innerHTML="<button style='border:4px solid black'>apply</button>";
+   valur.id="knopka";
+  // var valur=document.createElement('button');
+   
+    alert("first");
+    poz.document.getElementsByClassName("down");
+    
+     alert(poz);
+    alert("trabaja?");
+   poz.insertAdjacentElement("beforeend",valur);
+   alert("ff");
+}
+   /*
+  poz.insertAdjacentElement("beforeEnd",valur);
+    alert("no");
+   alert(poz.insertAdjacentElement);*/
+   //poz.insertAdjacentElement("beforeend","<button style='border:4px solid black'>apply</button>");
