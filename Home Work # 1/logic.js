88061bdff3afd32d4fab207b924251b39c5d30cd
Roman <yreymak@gmail.com>
2018-10-23 04:01:38 +0300

    создание кнопок

--- Home Work # 1/logic.js
+++ Home Work # 1/logic.js
@@ -1,41 +1,22 @@
-
-/*function processing(){
-    var tempt = document.getElementById("inpur").value;
-    return tempt;
-    
+function output(){
+ var temp=document.getElementById("inpur").value;
+  return temp;
 }
 
-function createBut(){
-count="c";
-var value = document.createElement("<button >apply</button>");
-value.id=count;
-var colo=document.getElementById("down");
-colo.insertAdjacentElement("beforeEnd",value);
-}
-
-
-"<button style='border:4px solid black'>apply</button>"
-
-*/
-function tr(){
-   alert("start");
-   poz.document.getElementsByClassName("down");
-   var valur=document.createElement('button');
-   //var valur=document.createElement('div');
-   valur.innerHTML="<button style='border:4px solid black'>apply</button>";
-   valur.id="knopka";
-  // var valur=document.createElement('button');
+function createButton(color){
+  var colour=color; //для хранения значения цвета для другой ф-ции
+    var div =document.getElementById("down");
+    var butt = document.createElement("button");
+    butt.id="knopka";
+    butt.innerHTML="применить";
    
-    alert("first");
-    poz.document.getElementsByClassName("down");
-    
-     alert(poz);
-    alert("trabaja?");
-   poz.insertAdjacentElement("beforeend",valur);
-   alert("ff");
+    butt.style=" width:100px;height:70px; border-top-left-radius: 100% 20px; border-bottom-right-radius: 100% 20px;background: "+color;
+    div.insertAdjacentElement("beforeend",butt);
+
+  
 }
-   /*
-  poz.insertAdjacentElement("beforeEnd",valur);
-    alert("no");
-   alert(poz.insertAdjacentElement);*/
-   //poz.insertAdjacentElement("beforeend","<button style='border:4px solid black'>apply</button>");
+
+function apply(colot){        //эта функция на будущее 
+  var templ=document.getElementById("entire");
+    templ.style.background=colot;
+}
