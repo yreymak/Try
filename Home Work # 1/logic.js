f1e33bfa28149f24c8c67fcf8337d95caa916a9b
Roman <yreymak@gmail.com>
2018-10-29 22:24:30 +0200

    bags were fixed

--- Home Work # 1/logic.js
+++ Home Work # 1/logic.js
@@ -1,5 +1,8 @@
-//color=document.getElementById('input').value;;
+var counter = 0;
 
+
+
+////////////////////////////
 //changeBackground
 function changeBackground(color) {
   
@@ -7,6 +10,9 @@ function changeBackground(color) {
 
   mainContainer.style.background = color;
 }
+
+/////////////////////////////
+///////////creating Main-Button
 
 function createButton(color) {
   if(ValidImage(color)){
@@ -21,55 +27,98 @@ function createButton(color) {
 }
 }
 
+
+
+///////////////////////////////////
+///////creating helpful-Button
 function create_helpful_Button(text){ 
+  
    let button = document.createElement("button");
+
    button.style.height=14;
    button.textContent=text;
+   button.id=text;;
+ 
    let place = document.getElementById("input");
    place.insertAdjacentElement("afterend",button);
    return button;
 }
 
-function add_delete_button(obj,main_button,buttonCon){
+
+
+
+/////////////////////////
+///Delete-Button
+function add_delete_button(obj,main_button,buttonContainer){
   obj.addEventListener("click",function(){
-    buttonCon.removeChild(main_button);
-    obj.parentNode.removeChild(this);
+
+    buttonContainer.removeChild(main_button);
+    
+    Remove_Elements_by_ID("delete");
+    Remove_Elements_by_ID("select");
+    counter--;
+    
   });
 }
 
 
+
+/////////////////////////////////
+/////////add_change_Background_button
 function add_change_Background_button(obj,color){
   obj.addEventListener("click",function(){
   changeBackground(color) ;
-   
-  obj.parentNode.removeChild(this);
+
+  Remove_Elements_by_ID("select");
+  Remove_Elements_by_ID("delete");
+  counter--;
+  
+  
   });
 }
 
 
 
+////////////////////////////
+//////////Adding Main-Button
 function addButton() {
   var color =  document.getElementById('input').value;
 
   var button_main = createButton(color);
-  
-  
-  button_main.addEventListener('click',function(){
-    add_change_Background_button(create_helpful_Button("select"),color)
-    add_delete_button(create_helpful_Button("delete"),button_main,buttonContainer);
-  });
-
-  
   var buttonContainer = document.getElementById('button-container');
   buttonContainer.appendChild(button_main);
-  //buttonContainer.insertAdjacentElement("beforeend",button_main);
+  
+  button_main.addEventListener('click',button_control);
+  
+
+  function button_control(){
+   if(counter===0){
+    add_change_Background_button(create_helpful_Button("select"),color)
+    add_delete_button(create_helpful_Button("delete"),button_main,buttonContainer);
+    counter++;
+  }
+  else{
+    Remove_Elements_by_ID("select");
+    Remove_Elements_by_ID("delete");
+    counter--;
+    button_control();
+
+  }
+}  
+  
+  
 }
 
-//////////////////////////////////////////////////////
-//////__delete__////////////////////////////////////
-function obj_delete(obj){
-   delete obj;
-}
+
+
+
+function Remove_Elements_by_ID(ID){
+ 
+  var element = document.getElementById(ID);
+  element.parentNode.removeChild(element);
+  }
+
+
 
 
 
